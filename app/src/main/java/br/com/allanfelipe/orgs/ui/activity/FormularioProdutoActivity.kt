f9561c268e8c4b4ce616c07391bbd368a2309460
Allan <allanfelipedk@gmail.com>
2023-12-11 07:05:13 -0300

    creating a new activity with android studio and configuring build.gradle

---
+++ app/src/main/java/br/com/allanfelipe/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,9 @@
+package br.com.allanfelipe.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import br.com.allanfelipe.orgs.R
+
+class FormularioProdutoActivity :
+    AppCompatActivity(R.layout.activity_formulario_produto) {
+
+}
--- app/src/main/java/br/com/allanfelipe/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/allanfelipe/orgs/ui/activity/MainActivity.kt
@@ -1,25 +1,17 @@
 package br.com.allanfelipe.orgs.ui.activity
 
+import android.app.Activity
 import android.os.Bundle
-import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.allanfelipe.orgs.R
 import br.com.allanfelipe.orgs.model.Produto
 import br.com.allanfelipe.orgs.ui.recyclerview.adapter.ListaDeProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.text = "Cesta De Frutas"
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.text = "Laranja, Maça e manga"
-//        val valor = findViewById<TextView>(R.id.valor)
-//        valor.text = "19.99"
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
         recyclerView.adapter = ListaDeProdutosAdapter(
             context = this, produto = listOf(
@@ -38,6 +30,7 @@ class MainActivity : AppCompatActivity() {
                     descricao = "teste desc 2 ",
                     valor = BigDecimal("39.99")
                 )
-            ))
+            )
+        )
     }
 }
