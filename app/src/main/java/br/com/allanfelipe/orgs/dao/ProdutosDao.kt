e8c3723091cea20dc945ab02a452af67e540eee0
Allan <allanfelipedk@gmail.com>
2023-12-13 20:13:43 -0300

    refactoring the code

--- app/src/main/java/br/com/allanfelipe/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/br/com/allanfelipe/orgs/dao/ProdutosDao.kt
@@ -7,11 +7,13 @@ class ProdutosDao {
     fun adiciona(produto: Produto){
         produtos.add(produto)
     }
-    fun buscaTodos() : List<Produto>{
+
+    fun buscaTodos() : List<Produto> {
         return produtos.toList()
     }
 
     companion object {
         private val produtos = mutableListOf<Produto>()
     }
+
 }
--- app/src/main/java/br/com/allanfelipe/orgs/ui/recyclerview/adapter/ListaDeProdutosAdapter.kt
+++ app/src/main/java/br/com/allanfelipe/orgs/ui/recyclerview/adapter/ListaDeProdutosAdapter.kt
@@ -9,34 +9,43 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.allanfelipe.orgs.R
 import br.com.allanfelipe.orgs.model.Produto
 
-class ListaDeProdutosAdapter(
+class ListaProdutosAdapter(
     private val context: Context,
-    private val produto: List<Produto>
-) : RecyclerView.Adapter<ListaDeProdutosAdapter.ViewHolder>() {
+    produtos: List<Produto>
+) : RecyclerView.Adapter<ListaProdutosAdapter.ViewHolder>() {
+
+    private val produtos = produtos.toMutableList()
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
         fun vincula(produto: Produto) {
-            val nome = itemView.findViewById<TextView>(R.id.nome)
+            val nome = itemView.findViewById<TextView>(R.id.produto_item_nome)
             nome.text = produto.nome
-
-            val descricao = itemView.findViewById<TextView>(R.id.descricao)
+            val descricao = itemView.findViewById<TextView>(R.id.produto_item_descricao)
             descricao.text = produto.descricao
-
-            val valor = itemView.findViewById<TextView>(R.id.valor)
+            val valor = itemView.findViewById<TextView>(R.id.produto_item_valor)
             valor.text = produto.valor.toPlainString()
         }
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(context)
-        val view = layoutInflater.inflate(R.layout.produtio_item, parent, false)
+        val inflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.produto_item, parent, false)
         return ViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val produto = produto[position]
+        val produto = produtos[position]
         holder.vincula(produto)
     }
 
-    override fun getItemCount(): Int = produto.size
+    override fun getItemCount(): Int = produtos.size
+
+    fun atualiza(produtos: List<Produto>) {
+        this.produtos.clear()
+        this.produtos.addAll(produtos)
+        notifyDataSetChanged()
+    }
+
 }
