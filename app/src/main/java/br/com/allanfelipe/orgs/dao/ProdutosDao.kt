641ca254d6462eec6fe82201a2ae0c8274bf2f33
Allan <allanfelipedk@gmail.com>
2023-12-18 20:14:19 -0300

    customizing the product item by changing the font and using alternative drawables

--- app/src/main/java/br/com/allanfelipe/orgs/dao/ProdutosDao.kt
+++ app/src/main/java/br/com/allanfelipe/orgs/dao/ProdutosDao.kt
@@ -1,19 +1,27 @@
 package br.com.allanfelipe.orgs.dao
 
 import br.com.allanfelipe.orgs.model.Produto
+import java.math.BigDecimal
 
 class ProdutosDao {
 
-    fun adiciona(produto: Produto){
+    fun adiciona(produto: Produto) {
         produtos.add(produto)
     }
 
-    fun buscaTodos() : List<Produto> {
+    fun buscaTodos(): List<Produto> {
         return produtos.toList()
     }
 
     companion object {
-        private val produtos = mutableListOf<Produto>()
+        private val produtos = mutableListOf<Produto>(
+            Produto(
+                nome = "Salada De Fruta",
+                descricao = "Laranja, maça e uva",
+                valor = BigDecimal("19.99")
+            )
+
+        )
     }
 
 }
